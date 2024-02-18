2cac31e26eab9222a9ad4a719c6889b6a101c27b
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-02-18 01:22:41 -0300

    Styles do Tech

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,18 +14,25 @@ const HomePage = () => {
           Olá, sou Daniel Trindade, apaixonado por tecnologia e dedicado à programação web. Graduado em Ciência e Tecnologia pela Universidade Federal do Rio Grande do Norte, atualmente cursando o 7° período de Engenharia da Computação na mesma instituição. Sou um profissional criativo, de rápido aprendizado e que aprecia desafios.
         </p>
       </div>
-      <div className={styles.tecContainer}>
+      <div className={styles.techContainer}>
         <h1>Tecnologias</h1>
-        <TechBox
-          title='Front-End'
-          text='Desenvolvo sites responsivos, componentizados e dotados de tecnologia atuais em conjunto com bibliotecas populares tais como bootstrap.'
-          techs={['React', 'HTML', 'CSS']}
-        />
-        <TechBox
-          title='Back-End'
-          text='Desenvolvo sites responsivos, componentizados e dotados de tecnologia atuais em conjunto com bibliotecas populares tais como bootstrap.'
-          techs={['JavaScript', 'PHP']}
-        />
+        <div className={styles.tech}>
+          <TechBox
+            title='Front-End'
+            text='Desenvolvo sites responsivos, componentizados e dotados de tecnologia atuais em conjunto com bibliotecas populares tais como bootstrap e Konva.'
+            techs={['React', 'HTML', 'CSS']}
+          />
+          <TechBox
+            title='Back-End'
+            text='Ápto a promover integração entre Banco de Dados e o Front de forma segura e otimizada com bom domínio da lógica de programação.'
+            techs={['JavaScript', 'PHP', 'MySQL']}
+          />
+          <TechBox
+            title='Outros'
+            text='Durante o processo de formação na Universidade pude desenvolver alguns projetos que me deram fluencia nas seguintes linguagens:'
+            techs={['C++', 'C', 'QT', 'Python']}
+          />
+        </div>
       </div>
     </div>
   )
