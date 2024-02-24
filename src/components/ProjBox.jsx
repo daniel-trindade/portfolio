9ef791e69055ef737948dc9fe8f59547c8cf64ca
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-02-23 22:40:06 -0300

    implantação do Transition

--- src/components/ProjBox.jsx
+++ src/components/ProjBox.jsx
@@ -1,18 +1,38 @@
+import { useState } from 'react'
+
 import styles from './ProjBox.module.css'
 
 const ProjBox = ({img, title, techs, description}) =>{
 
+  const [showDescription, setShowDescription] = useState(false)
+
   return(
-    <div className={styles.projContainer}>
-      <img src={img} alt="imagem de capa do projeto" />
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <h2>Tecnologias Utilizadas:</h2>
-      <ul className={styles.list}>
-        {techs.map((tech, index) => (
-          <li key={index} className={styles.listItem}>{tech}</li>
-        ))}
-      </ul>
+    <div 
+      className={styles.projContainer} 
+      onMouseEnter={() => setShowDescription(true)}
+      onMouseLeave={() => setShowDescription(false)}
+    >
+
+
+      {!showDescription ? (
+        <img src={img} alt="imagem de capa do projeto" />
+      ) : (
+        <div>
+          <h1>{title}</h1>
+          <p>{description}</p>
+          <h2>Tecnologias Utilizadas:</h2>
+          <ul className={styles.list}>
+            {techs.map((tech, index) => (
+              <li key={index} className={styles.listItem}>{tech}</li>
+            ))}
+          </ul>
+        </div>
+
+
+      )}
+      
+
+
     </div>
   )
   
