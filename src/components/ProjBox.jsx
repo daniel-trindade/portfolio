b5b594763f3a6b8e37ed2f356d744934b09e6507
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-02-23 22:52:57 -0300

    update

--- src/components/ProjBox.jsx
+++ src/components/ProjBox.jsx
@@ -7,32 +7,23 @@ const ProjBox = ({img, title, techs, description}) =>{
   const [showDescription, setShowDescription] = useState(false)
 
   return(
-    <div 
-      className={styles.projContainer} 
-      onMouseEnter={() => setShowDescription(true)}
-      onMouseLeave={() => setShowDescription(false)}
-    >
-
-
-      {!showDescription ? (
-        <img src={img} alt="imagem de capa do projeto" />
-      ) : (
-        <div>
-          <h1>{title}</h1>
-          <p>{description}</p>
-          <h2>Tecnologias Utilizadas:</h2>
-          <ul className={styles.list}>
-            {techs.map((tech, index) => (
-              <li key={index} className={styles.listItem}>{tech}</li>
-            ))}
-          </ul>
-        </div>
-
-
-      )}
-      
-
-
+    <div className={styles.projContainer} onMouseEnter={() => setShowDescription(true)} onMouseLeave={() => setShowDescription(false)}>
+      <h1>{title}</h1>
+      <div className={styles.transition}>
+        {!showDescription ? (
+          <img src={img} alt="imagem de capa do projeto" />
+        ) : (
+          <div>
+            <p>{description}</p>
+            <h2>Tecnologias Utilizadas:</h2>
+            <ul className={styles.list}>
+              {techs.map((tech, index) => (
+                <li key={index} className={styles.listItem}>{tech}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   )
   
