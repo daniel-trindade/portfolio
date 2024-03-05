657c38d2a4a16a93f42bf2782065c7f4ec3060a4
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-03-05 00:22:41 -0300

    skills apdate

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,14 +13,42 @@ const Skills = () =>{
         <div className={styles.sideContainer}>
           <h3>Soft Skills</h3>
           <div className={styles.tech}>
-    
+            <div className={styles.box}>
+              <h3>Inglês</h3>
+              <p>
+                <span>Writing:</span> Intermediário - <span>Listening:</span> Avançado - <span>Speaking:</span> Avançado. <br />
+              </p>
+            </div>
           </div>
         </div>
 
         <div className={styles.sideContainer}>
           <h3>Tecnologias</h3>
           <div className={styles.tech}>
-
+            <TechBox
+              tech="HTML"
+              lvlExp="2"
+            />
+            <TechBox
+              tech="CSS"
+              lvlExp="2"
+            />
+            <TechBox
+              tech="JavaScript"
+              lvlExp="2"
+            />
+            <TechBox
+              tech="React"
+              lvlExp="2"
+            />
+            <TechBox
+              tech="C++"
+              lvlExp="1"
+            />
+            <TechBox
+              tech="Python"
+              lvlExp="1"
+            />
           </div>
         </div>
 
