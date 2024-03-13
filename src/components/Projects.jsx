6132712c43b24358b876a7211749eaf51f84baf4
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-03-12 23:43:22 -0300

    vanilla

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,20 @@
 import styles from './Projects.module.css'
 import ProjectSlideBox from './ProjectSlideBox'
+import VanillaTilt from 'vanilla-tilt'
 
 const Projects = () =>{
+
+  VanillaTilt.init(document.querySelector("#tilt"));
 
   return(
     <div className={styles.projectsContainer}>
       <h2 className={styles.sectionTitle}>Projetos de Estudo Pessoal</h2>
-      <ProjectSlideBox/>
+
+      <div className={styles.vanilaTilt} id='tilt'>
+        <ProjectSlideBox/>  
+      </div> 
+
+      <script type="text/javascript" src="./VanillaTilt.js"></script>
     </div>
   )
 }
