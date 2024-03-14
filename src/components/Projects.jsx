21e21c72a2add0327b7084af2f5904de78a1d744
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-03-13 23:06:33 -0300

    Revert "vanilla"
    This reverts commit 6132712c43b24358b876a7211749eaf51f84baf4.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,12 @@
 import styles from './Projects.module.css'
 import ProjectSlideBox from './ProjectSlideBox'
-import VanillaTilt from 'vanilla-tilt'
 
 const Projects = () =>{
-
-  VanillaTilt.init(document.querySelector("#tilt"));
 
   return(
     <div className={styles.projectsContainer}>
       <h2 className={styles.sectionTitle}>Projetos de Estudo Pessoal</h2>
-
-      <div className={styles.vanilaTilt} id='tilt'>
-        <ProjectSlideBox/>  
-      </div> 
-
-      <script type="text/javascript" src="./VanillaTilt.js"></script>
+      <ProjectSlideBox/>
     </div>
   )
 }
