70dd4621cf67af3f9ec8eb582919d9f62941ac8a
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-03-16 02:12:59 -0300

    responsividade da barra de navegação

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,20 @@
+import { FaHamburger } from "react-icons/fa";
+import { useState } from "react";
+
 import styles from './NavBar.module.css'
 
 const NavBar = () =>{
+
+  const [menuVisible, setMenuVisible] = useState(false)
+
+  const visible = () =>{
+    setMenuVisible(!menuVisible)
+  }
+
   return(
     <nav className={styles.navBar}>
-      <ul>
+      <a onClick={visible} href="#" className={styles.hamburgerMenu}><FaHamburger /></a>
+      <ul className={`${styles.menuItem} ${menuVisible ? styles.visible : styles.hidden}`}>
         <li><a href="#home">Início</a></li>
         <li><a href="#aboutMe">Sobre mim</a></li>
         <li><a href="#tech">Tecnologias e Habilidades</a></li>
