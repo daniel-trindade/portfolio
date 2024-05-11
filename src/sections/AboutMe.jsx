e74695f4a68050899deeeb16c592d800627c40b2
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-05-10 21:17:28 -0300

    Update AboutMe.jsx

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
             <div className={styles.txtContainer}>
               <h1>Sobre mim</h1>
               <p>
-                Sou um estusiasta da tecnologia e de tudo que ela tem a oferecer. Tenho me dedicado à programação web com enfase ao front-end sem deixar de lado boas oportunidades de  desenvolver minhas habilidades no back-end. Como profissional, destaco-me pela criatividade, capacidade de aprendizado acelerado e uma propensão natural para enfrentar e superar desafios. 
+                Sou um entusiasta da tecnologia e de tudo que ela tem a oferecer. Tenho me dedicado à programação web com ênfase ao front-end sem deixar de lado boas oportunidades de  desenvolver minhas habilidades no back-end. Como profissional, destaco-me pela criatividade, capacidade de aprendizado acelerado e uma propensão natural para enfrentar e superar desafios. 
               </p>
             </div>
 
